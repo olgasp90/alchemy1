0052337fdec24c54dfb5e6087718b547af94f881
Olga <olga_sprincean@yahoo.com>
2021-03-25 22:59:10 -0500

    add and handle posts

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 
@@ -15,13 +15,21 @@ debug = DebugToolbarExtension(app)
 
 
 connect_db(app)
-db.drop_all()
 db.create_all()
 
 @app.route('/')
 def root():
-    return redirect('/users')
+    posts = Post.query.order_by(Post.created_at.desc()).limit(5).all()
+    return render_template('home.html', posts=posts)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Show 404 NOT FOUND page."""
+    return render_template('404.html'), 404
+
+
+########## User ##########
 @app.route('/users')
 def users_list():
     users = User.query.order_by(User.last_name, User.first_name).all()
@@ -40,6 +48,7 @@ def new_user():
     )
     db.session.add(new)
     db.session.commit()
+    flash(f"User {new.first_name} added")
     return redirect('/users')
 
 @app.route('/users/<int:user_id>')
@@ -61,6 +70,7 @@ def update_user(user_id):
 
     db.session.add(user)
     db.session.commit()
+    flash(f'User {user.first_name} {user.last_name} edited.')
 
     return redirect('/users')
 
@@ -70,5 +80,57 @@ def delete_user(user_id):
 
     db.session.delete(user)
     db.session.commit()
+    flash(f'User {user.first_name} {user.last_name} deleted.')
 
     return redirect('/users')
+
+
+######### Posts ##########
+@app.route('/users/<int:user_id>/posts/new')
+def new_post_form(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('post_form.html', user=user)
+
+
+@app.route('/users/<int:user_id>/posts/new', methods=["POST"])
+def new_post(user_id):
+    user = User.query.get_or_404(user_id)
+    post = Post(title=request.form['title'], content=request.form['content'], user=user)
+
+    db.session.add(post)
+    db.session.commit()
+    flash(f"Post {post.title} added.")
+    return redirect('/')
+
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post_show.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit')
+def edit_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post_edit.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=['POST'])
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+    flash('Your changes were saved!')
+
+    return redirect('/')
+
+@app.route('/posts/<int:post_id>/delete', methods=['POST'])
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post was deleted successfully!')
+
+    return  redirect('/')
--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 """Models for Blogly."""
+import datetime
 #import sql
 from flask_sqlalchemy import SQLAlchemy
+
 
 #execute sql alchemy
 db = SQLAlchemy()
@@ -8,6 +10,7 @@ db = SQLAlchemy()
 def connect_db(app):
     db.app = app
     db.init_app(app)
+
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -23,5 +26,26 @@ class User(db.Model):
 
     image_url = db.Column(db.String, default='No image')
 
+    posts = db.relationship('Post', backref='user', cascade='all, delete-orphan')
+
     def __repr__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    title = db.Column(db.String, nullable=False)
+
+    content = db.Column(db.Text,nullable=False)
+
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    @property
+    def friendly_date(self):
+        """Format date"""
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
